db2d1064864858ff6367be554b0993853211646d
Janik Münzenberger <janik-muenzenberger@web.de>
2018-04-22 21:16:08 +0200

    Api to get the diseases

--- src/backend/controller/disease.js
+++ src/backend/controller/disease.js
@@ -1,5 +1,41 @@
 const router  = require('express').Router();
+const Disease = require('../model/disease');
+const logger = require('winston');
 
-router.get('/', (req,res)=>{});
+/** Get all diseases with name and id */
+router.get('/', getDiseases);
+
+/** Get single disease y id */
+router.get('/:id', getDisease);
+
+
+function getDiseases(req, res){
+    Disease.find({}, ["id", "name", "symptoms"])
+        .then(diseases => {
+            res.status(200);
+            res.send({
+                success: true,
+                data: diseases
+            });
+        }).catch(err => {
+
+        });
+}
+
+function getDisease(req, res){
+    Disease.findById(req.params.id).select("-__v")
+        .then(disease => {
+            res.status(200);
+            res.send({
+                success: true,
+                data: disease
+            });
+        }).catch(err => {
+            res.status(400);
+            res.send(err);
+            
+            logger.error(err);
+        });
+}
 
 module.exports = router;
