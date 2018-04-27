e9d09e2d26210875e1b9356a2d04ced4e18855a4
Janik Münzenberger <janik-muenzenberger@web.de>
2018-04-27 21:48:32 +0200

    Added schema methods

--- src/backend/model/user.js
+++ src/backend/model/user.js
@@ -4,53 +4,50 @@ const crypt = require('password-hash');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 6
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8
-  },
-  email: {
-    type: String,
-    unique: true,
-    required:true
-  },
-  image_url: {
-    type: String,
-  },
-  jobs: [
-    {
-      type: mongoose.Schema.Types.ObjectId, ref:'Job'
+    name: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    image_url: {
+        type: String,
+    },
+    jobs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Job'
+    }]
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
     }
-  ]
-},
-{
-  timestamps:
-  {
-      createdAt: 'created_at',
-      updatedAt: 'updated_at'
-  }
 });
 
-
-
-userSchema.methods.verifyPassword = function(password, next) {
-  return crypt.verify(password, this.password);
+/** Verifies raw password with db hash */
+userSchema.methods.verifyPassword = function (password, next) {
+    return crypt.verify(password, this.password);
 };
-
-userSchema.pre('save', function(next) {
-  this.password = crypt.generate(this.password);
-  next();
+/** Encrypts the password before saving */
+userSchema.pre('save', function (next) {
+    this.password = crypt.generate(this.password);
+    next();
+});
+/** If not already encrypted: Encrypt the password before updating */
+userSchema.pre('update', function (next) {
+    if (!crypt.isHashed(this.password)) {
+        this.password = crypt.generate(this.password);
+    }
+    next();
 });
 
-userSchema.pre('update', function(next) {
-  console.log(this.password);
-  next();
-});
-
-let model = mongoose.model("User", userSchema);
-module.exports = model;
+module.exports = mongoose.model("User", userSchema);
