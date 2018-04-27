d56356a61fe1200384924fd5fb5fa32663151e73
Janik Münzenberger <janik-muenzenberger@web.de>
2018-04-27 21:49:15 +0200

    Created useful middleware for authentication and parameter checking

--- src/backend/lib/middleware.js
+++ src/backend/lib/middleware.js
@@ -1,75 +1,133 @@
 const verifyJWTToken = require('./auth').verifyJWTToken;
 const errors = require('../lib/errors');
 const winston = require('winston');
+const User = require('../model/user');
 
-function checkQuery(params){
+/** Checks Query fields for required fields 
+ * strictMode: only allow the given fields, no others
+*/
+function checkQuery(params, strictMode = false) {
     return (req, res, next) => {
         try {
-            checkRequiredKeys(req.query, params);
+            checkRequiredKeys(req.query, params, strictMode);
             next();
-        }catch(err){
+        } catch (err) {
+            errors.sendError(res, err);
+        }
+    };
+}
+/** Checks Body fields for required fields 
+ * strictMode: only allow the given fields, no others
+*/
+function checkBody(params, strictMode = false) {
+    return (req, res, next) => {
+        try {
+            checkRequiredKeys(req.body, params, strictMode);
+            next();
+        } catch (err) {
             errors.sendError(res, err);
         }
     };
 }
 
-function checkBody(params){
+/** Checks Header fields for required fields 
+ * strictMode: only allow the given fields, no others
+*/
+function checkHeaders(params, strictMode = false) {
     return (req, res, next) => {
         try {
-            checkRequiredKeys(req.body, params);
+            checkRequiredKeys(req.headers, params, strictMode);
             next();
-        }catch(err){
+        } catch (err) {
             errors.sendError(res, err);
         }
     };
 }
 
-function checkHeaders(params){
-    return (req, res, next) => {
-        try {
-            checkRequiredKeys(req.headers, params);
-            next();
-        }catch(err){
-            errors.sendError(res, err);
-        }
-    };
-}
+/** Checks Object fields for required fields 
+ * strictMode: only allow the given fields, no others
+*/
+function checkRequiredKeys(base, params, strictMode=false) {
+    if (!Array.isArray(params)) params = [params];
 
-function checkRequiredKeys(base, params){
-    if(!Array.isArray(params)) params = [params];
-    
+    if (strictMode && Object.keys(base).length > params.length) throw new errors.InputError('Unknown fields are set', 'Too many declarations');
+
     params.forEach(key => {
-        if(!base[key]) {
-            throw new errors.OnResponseError(`The field ${key} is required!`, "Missing Options");  
+        if (!base[key] && base[key] !== false) {
+            throw new errors.InputError(`The field ${key} is required!`, "Missing Options");
         }
     });
     return true;
 }
 
-function verifyJWT_MW(req, res, next){
+/** Middleware for verifying the JWT as Bearer Token in the "Authorization" Field of the header 
+ * Sets the encrypted data 
+*/
+function verifyJWT_MW(req, res, next) {
     let auth = req.headers.authorization;
-    
-    if(!auth || auth.split(' ')[0] != "Bearer"){
+
+    if (!auth || auth.split(' ')[0] != "Bearer") {
         let notSetError = new errors.AuthError("No bearer token in headers provided!");
         errors.sendError(res, notSetError);
-    }else{
+    } else {
         let token = auth.split(' ')[1];
-        
+
         verifyJWTToken(token)
-        .then( decodedToken => {
-            req.user = decodedToken.data;
-            next();
-        }).catch(err => {
-            let invalidError = new errors.AuthError("Invalid bearer token!");
-            errors.sendError(res, invalidError);
-            winston.error(err);
-        });
+            .then(decodedToken => {
+                req.tokenData = decodedToken.data;
+                next();
+            }).catch(err => {
+                let invalidError = new errors.AuthError("Invalid bearer token!");
+                errors.sendError(res, invalidError);
+                winston.error(err);
+            });
     }
+
+}
+
+/** Verifies the provided login data
+ * If token is provided search user by the tokens data
+ * If no token is provided: Email and Passwort are required fields in body
+ * 
+ * Tries to find user in DB 
+ *      if existing: req.user = user
+ *      else: Authentication Error
+ */
+function verifyLoginData(req, res, next) {
+    let search;
     
+    try {
+        if (!req.tokenData) {
+            checkRequiredKeys(req.body, ["email", "password"]);
+            search = {
+                email: req.body.email
+            };
+        } else {
+            checkRequiredKeys(req.body, ["password"]);
+            search = req.tokenData;
+        }
+        
+        User.findOne(search)
+            .then(user => {
+                if(!user){
+                    throw new errors.AuthError('No user with this credentials');
+                }else if (!user.verifyPassword(req.body.password)) {
+                    throw new errors.AuthError('Invalid Password');
+                }
+                req.user = user;
+                next();
+            }).catch(err => {
+                errors.sendError(res, err);
+            });
+
+    } catch (err) {
+        errors.sendError(res, err);
+    }
 }
 
 module.exports = {
     checkBody,
     checkQuery,
-    verifyJWT_MW
+    verifyJWT_MW,
+    verifyLoginData
 };
