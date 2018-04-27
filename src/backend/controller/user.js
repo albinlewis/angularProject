1d296e7f004cb1be106e09315dbac538f957858f
Janik Münzenberger <janik-muenzenberger@web.de>
2018-04-27 21:47:47 +0200

    Created authentication controllers and user usabilities

--- src/backend/controller/user.js
+++ src/backend/controller/user.js
@@ -1,119 +1,30 @@
-const ValidationError = require('mongoose').ValidationError;
-const auth = require('../lib/auth');
-const User = require('../model/user');
-const hasher = require('password-hash');
-const emailValidator = require('email-validator');
 const winston = require('winston');
+
+const User = require('../model/user');
 const errors = require('../lib/errors');
 
-function register(req, res){
-    let newUser = new User();
-    
-    try{
-        newUser.name = req.body.name;
+// ToDo: Implement profile update
+function update(req, res) {
 
-        if(!emailValidator.validate(req.body.email)){
-            throw "Please enter valid email";
-        }
-        
-        newUser.email = req.body.email;
+}
 
-        if(req.body.password.length < 8){
-            throw "Password must be 8 or more characters";
-        }
-        newUser.password = req.body.password;
-    }
-    catch(err){
-        res.status(400);
-        res.send({
-            success: false,
-            error: err
-        });
-        return;
-    }
-
-    newUser.save()
-        .then(user => {
-
-            res.status(201);
+/** Remove a user */
+function remove(req, res) {
+    let user = req.user;
+    user.remove()
+        .then(result => {
             res.send({
                 success: true,
-                message: `User ${user.email} successfully created`,
-                token: setToken(user)
+                message: "User successfully deleted"
             });
-        }).catch(err => {
-            res.status(400);
-            res.send(err);
-        });
-}
-
-function login(req, res){
-    User.findOne({email: req.body.email})
-        .then( user => {
-            
-            if(user.verifyPassword(req.body.password)){
-                
-                res.send({
-                    success: true,
-                    message: "You successfully logged in!",
-                    token: setToken(user)
-                });
-                
-            }else{
-                throw "Passwort is not valid.";
-            }
-        }).catch(err => {
-            winston.error(err);
-            res.status(400);
-            res.send({
-                success: false,
-                error: err
-            });
-        });
-}
-
-function update(req, res){
-    
-}
-
-function remove(req, res){
-    let user = req.user;
-    let password = req.body.password;
-    User.findOne({_id: user.id, email: user.email})
-        .then(user => {
-            if(!user){
-                throw new errors.OnResponseError('No valid user to token!');
-            }
-            else if(user.verifyPassword(password)){
-                user.remove()
-                    .then( result => {
-                        res.send({
-                            success: true,
-                            message: "User successfully deleted"
-                        });
-                    })
-                    .catch( err => {throw err; });
-            }else{
-                throw new errors.AuthError('Invalid Password!');
-            }
-        }).catch(err => {
+        })
+        .catch(err => {
             errors.sendError(res, err);
+            winston.error(err);
         });
-}
-
-function setToken(user){
-    
-    let token = auth.createJWTToken({
-            data: { 
-                id: user.id,
-                email: user.email
-            }
-        });
-    return token;
 }
 
 module.exports = {
-    login,
-    register,
+    update,
     remove
 };
--- src/backend/lib/auth.js
+++ src/backend/lib/auth.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-function verifyJWTToken(token){
+/** Verifies the Token by with secret from environment or config and return decoded token */
+function verifyJWTToken(token) {
     return new Promise((resolve, reject) => {
-        jwt.verify(token, process.env.JWT_SECRET || config.auth.secret, (err, decodedToken) => 
-        {
-            if (err || !decodedToken)
-            {
+        jwt.verify(token, process.env.JWT_SECRET || config.auth.secret, (err, decodedToken) => {
+            if (err || !decodedToken) {
                 return reject(err);
             }
 
@@ -14,12 +13,12 @@ function verifyJWTToken(token){
         });
     });
 }
+/** Creates a JWT Token of details by signing it with secret from environment or config */
+function createJWTToken(details) {
+    if (typeof details !== 'object') details = {};
+    if (!details.maxAge || typeof details !== 'number') details.maxAge = config.auth.maxAge;
+    if (!details.data || typeof details.data !== 'object') details.data = {};
 
-function createJWTToken(details){
-    if(typeof details !== 'object') details = {};
-    if(!details.maxAge || typeof details !== 'number') details.maxAge = config.auth.maxAge;
-    if(!details.data || typeof details.data !== 'object') details.data = {};
-    
 
     let token = jwt.sign({
         data: details.data
@@ -27,7 +26,7 @@ function createJWTToken(details){
         expiresIn: details.maxAge,
         algorithm: 'HS256'
     });
-    
+
     return token;
 }
 
