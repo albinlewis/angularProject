12d1198acb1c8e96d63f52a644375b9f2a8e6d8a
Janik Münzenberger <janik-muenzenberger@web.de>
2018-04-27 21:48:14 +0200

    Added some tests for user

--- src/backend/test/user.js
+++ src/backend/test/user.js
@@ -9,85 +9,81 @@ const passwordHash = require('password-hash');
 const server = require("../server");
 const User = require("../model/user");
 const examples = require("./examples");
+
 /** chai.js plugins */
 chai.use(chaiHttp);
 
 
 /** Tests */
-describe("Users", () => {
-      // Clear the database before each run in this block
-      beforeEach(done => {
+describe("User", () => {
+    // Clear the database before each run in this block
+    beforeEach(done => {
         User.remove({}, () => {
-          done();
+            done();
         });
-      });
-
-      /**
-       * Test the /POST register route
-       */
-      describe("/Post register", () => {
-        it("should GET a auth token", done => {
-          chai
-            .request(server)
-            .post("/api/register")
-            .send(examples.user)
-            .end((err, res) => {
-              res.should.have.status(201);
-              res.body.success.should.eq(true);
-              res.body.should.have.property('token');
-              done();
-            });
-        });
-      });
-
-      /**
-       * Test the /POST login route
-       */
-      describe("/Post login", () => {
-        it("should GET a auth token", done => {
-          let user = new User(examples.user);
-          user.save((err, user) => {
-            chai
-              .request(server)
-              .post("/api/login")
-              .send({
-                email: examples.user.email,
-                password: examples.user.password
-              })
-              .end((err, res) => {
-                res.should.have.status(200);
-                res.body.success.should.eq(true);
-                res.body.should.have.property('token');
-                done();
-              });
-          });
-        });
+    });
 
 
-        /**
-         * Test the /DELETE user route
-         */
-        describe("/Delete user", () => {
-          it("should return success true", done => {
+    /**
+     * Test the /DELETE user route
+     */
+    describe("/DELETE user", () => {
+        it("Delete existing user: should return success true", done => {
             /** First register user */
             chai
-              .request(server)
-              .post("/api/register")
-              .send(examples.user)
-              .end((err, res) => {
-
-                chai
-                  .request(server)
-                  .delete("/api/user")
-                  .set("Authorization", "Bearer " + res.body.token)
-                  .send(examples.user)
-                  .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.success.should.eql(true);
-                    done();
-                    });
+                .request(server)
+                .post("/api/register")
+                .send(examples.user)
+                .end((err, res) => {
+                    chai
+                        .request(server)
+                        .delete("/api/user")
+                        .set("Authorization", "Bearer " + res.body.token)
+                        .send(examples.user)
+                        .end((err, res) => {
+                            res.should.have.status(200);
+                            res.body.success.should.eql(true);
+                            done();
+                        });
                 });
+        });
+
+        it("Try to delete without token: should return error", done => {
+            new User(examples.user).save(function (err, user) {
+                chai
+                    .request(server)
+                    .delete("/api/user")
+                    .send(examples.user)
+                    .end((err, res) => {
+                        res.should.have.status(400);
+                        res.body.success.should.eql(false);
+                        res.body.should.have.property("error");
+                        done();
+                    });
             });
+        });
+
+        it("Delete existing user with bad credentials: should return an error", done => {
+            chai
+                .request(server)
+                .post("/api/register")
+                .send(examples.user)
+                .end((err, res) => {
+                    chai
+                        .request(server)
+                        .delete("/api/user")
+                        .set("Authorization", "Bearer " + res.body.token)
+                        .send({
+                            email: examples.user.email,
+                            password: "Falsches Passwort"
+                        })
+                        .end((err, res) => {
+                            res.should.have.status(400);
+                            res.body.success.should.eql(false);
+                            res.body.should.have.property("error");
+                            done();
+                        });
+                });
         });
     });
 });
