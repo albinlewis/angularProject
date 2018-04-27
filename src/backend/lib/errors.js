f58389497844000ab73b2ddd9c639d6d9e7ae3f3
Janik Münzenberger <janik-muenzenberger@web.de>
2018-04-27 21:45:29 +0200

    Added Errorhandling

--- src/backend/lib/errors.js
+++ src/backend/lib/errors.js
@@ -1,3 +1,4 @@
+/** Base Error Class */
 class PseError {
     constructor(message, name="PseError", status=400, options={}){
         this.message = message;
@@ -6,19 +7,25 @@ class PseError {
         this.options = options;
     }
 }
-
+/** Error for Authentication */
 class AuthError extends PseError{
-    constructor(message, status=400, name="Authentication Error", options={}){
+    constructor(message, name="Authentication Error", status=400, options={}){
         super(message, name, status, options);
     }
 }
-
+/** Error on Response */
 class OnResponseError extends PseError{
-    constructor(message, status=400, name="Authentication Error", options={}){
+    constructor(message, name="Authentication Error", status=400, options={}){
         super(message, name, status, options);
     }
 }
-
+/** Error for invalid inputs/missing inputs */
+class InputError extends PseError{
+    constructor(message, name="Input Error", status=400, options={}){
+        super(message, name, status, options);
+    }
+}
+/** Sends errors to client */
 function sendError(res, err, status=400){
 
     let body = {
@@ -42,5 +49,6 @@ module.exports = {
     OnResponseError,
     PseError,
     AuthError,
+    InputError,
     sendError
 };
