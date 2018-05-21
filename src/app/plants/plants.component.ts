2ff072ab72833c70c46de61d664d1cc1466395ff
Janik Münzenberger <janik-muenzenberger@web.de>
2018-05-21 21:12:40 +0200

    Plants component to api

--- src/app/plants/plants.component.ts
+++ src/app/plants/plants.component.ts
@@ -1,91 +1,35 @@
 import {Component, OnInit} from '@angular/core';
 import {Route, Router} from '@angular/router';
 import {BrowseService} from '../services/browse.service';
+import { IPlant } from '../model/IPlant';
+import { PlantService } from '../services/plant.service';
 
 @Component({
     selector: 'app-plants',
     templateUrl: './plants.component.html',
-    styleUrls: ['./plants.component.css']
+    styleUrls: ['./plants.component.css'],
+    providers: [ PlantService ]
 })
 export class PlantsComponent implements OnInit {
 
-    searchedplant = '';
-    allplants = [{
-        name: 'List1',
-        description: 'taque verae amicitiae difficillime reperiuntur in iis qui in honoribus ' +
-        'reque publica versantur; ubi enim istum invenias qui honorem amici anteponat suo? Quid? Haec ut' +
-        ' omittam, quam graves, quam difficiles plerisque videntur calamitatum societates! Ad quas non est facile ' +
-        'inventu qui descendant. Quamquam Ennius recte.'
-    }
-        , {
-            name: 'List2',
-            description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam eget nisl. Aenean fermentum risus id tortor. Aliquam erat volutpat. Etiam neque. Integer lacinia. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Pellentesque sapien. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Praesent dapibus. Praesent id justo in neque elementum ultrices. Aliquam ante. Maecenas sollicitudin. Nam quis nulla. Nullam eget nisl.'
-        }
-        , {
-            name: 'List Item Heading 1',
-            description: 'taque verae amicitiae difficillime reperiuntur in iis qui in honoribus ' +
-            'reque publica versantur; ubi enim istum invenias qui honorem amici anteponat suo? Quid? Haec ut' +
-            ' omittam, quam graves, quam difficiles plerisque videntur calamitatum societates! Ad quas non est facile ' +
-            'inventu qui descendant. Quamquam Ennius recte.'
-        }
-        , {
-            name: 'List Item Heading 1',
-            description: 'taque verae amicitiae difficillime reperiuntur in iis qui in honoribus ' +
-            'reque publica versantur; ubi enim istum invenias qui honorem amici anteponat suo? Quid? Haec ut' +
-            ' omittam, quam graves, quam difficiles plerisque videntur calamitatum societates! Ad quas non est facile ' +
-            'inventu qui descendant. Quamquam Ennius recte.'
-        }
-        , {
-            name: 'List Item Heading 1',
-            description: 'taque verae amicitiae difficillime reperiuntur in iis qui in honoribus ' +
-            'reque publica versantur; ubi enim istum invenias qui honorem amici anteponat suo? Quid? Haec ut' +
-            ' omittam, quam graves, quam difficiles plerisque videntur calamitatum societates! Ad quas non est facile ' +
-            'inventu qui descendant. Quamquam Ennius recte.'
-        }
-        , {
-            name: 'List Item Heading 1',
-            description: 'taque verae amicitiae difficillime reperiuntur in iis qui in honoribus ' +
-            'reque publica versantur; ubi enim istum invenias qui honorem amici anteponat suo? Quid? Haec ut' +
-            ' omittam, quam graves, quam difficiles plerisque videntur calamitatum societates! Ad quas non est facile ' +
-            'inventu qui descendant. Quamquam Ennius recte.'
-        }
-        , {
-            name: 'List Item Heading 1',
-            description: 'taque verae amicitiae difficillime reperiuntur in iis qui in honoribus ' +
-            'reque publica versantur; ubi enim istum invenias qui honorem amici anteponat suo? Quid? Haec ut' +
-            ' omittam, quam graves, quam difficiles plerisque videntur calamitatum societates! Ad quas non est facile ' +
-            'inventu qui descendant. Quamquam Ennius recte.'
-        }
-        , {
-            name: 'List Item Heading 1',
-            description: 'taque verae amicitiae difficillime reperiuntur in iis qui in honoribus ' +
-            'reque publica versantur; ubi enim istum invenias qui honorem amici anteponat suo? Quid? Haec ut' +
-            ' omittam, quam graves, quam difficiles plerisque videntur calamitatum societates! Ad quas non est facile ' +
-            'inventu qui descendant. Quamquam Ennius recte.'
-        }
-        , {
-            name: 'List Item Heading 1',
-            description: 'taque verae amicitiae difficillime reperiuntur in iis qui in honoribus ' +
-            'reque publica versantur; ubi enim istum invenias qui honorem amici anteponat suo? Quid? Haec ut' +
-            ' omittam, quam graves, quam difficiles plerisque videntur calamitatum societates! Ad quas non est facile ' +
-            'inventu qui descendant. Quamquam Ennius recte.'
-        }
+    filterValue = '';
+    plants: IPlant[] = [];
 
-    ];
-
-
-    constructor(private route: Router, private browservice: BrowseService) {
+    constructor(private route: Router, private plantService: PlantService) {
         console.log('plants component created');
     }
 
     ngOnInit() {
+        this.getPlants();
     }
 
-    Onshowdetails(plants) {
-        this.route.navigate(['/details']);
-        this.browservice.plantselect = plants;
+    getPlants(){
+        this.plantService.getAllPlants()
+            .then(plants => this.plants = plants);
+    }
 
-
+    onShowDetails(plant: IPlant) {
+        this.route.navigate(['/details', plant._id]);
     }
 
 }
