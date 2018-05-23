15f83415fa9df9c395687e75fb0187e19f50e43f
albin <albingeraud@yahoo.fr>
2018-05-23 04:36:18 +0200

    fix margin and header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import * as $ from 'jquery';
 import {AuthService} from '../services/auth.service';
 import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
     selector: 'app-header',
@@ -10,7 +11,7 @@ import {Router} from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-    constructor(private authservice: AuthService,  private route: Router) {
+    constructor(private authservice: AuthService, private route: Router, public snackBar: MatSnackBar) {
     }
 
     ngOnInit() {
@@ -18,8 +19,12 @@ export class HeaderComponent implements OnInit {
 
 
     logout() {
-           this.authservice.logout();
-           this.route.navigate(['/login']);
+        this.authservice.logout();
+        this.snackBar.open('Logout', 'succeed', {
+            duration: 2000,
+        });
+        this.route.navigate(['/login']);
+
     }
 
 }
