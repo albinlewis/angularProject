c9e3e9f6beb214627d5b1aa56085a147490c8f89
Janik Münzenberger <janik-muenzenberger@web.de>
2018-05-23 15:39:16 +0200

    Added user

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { IUser } from '../model/IUser';
+
+@Injectable()
+export class UserService {
+
+  user: IUser = null;
+
+  constructor() {
+    let user = localStorage.getItem('user');
+    if(user) this.setUser(JSON.parse(user));
+  }
+
+  setUser(user: IUser){
+    this.user = user;
+    localStorage.setItem('user', JSON.stringify('user'));
+  }
+  
+  getUser(): IUser{
+    return this.user;
+  }
+
+  unsetUser(){
+    this.user = null;
+    localStorage.removeItem('user');
+  }
+
+}
