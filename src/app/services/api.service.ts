1e55539cc599a71245f507fccea64dce0d333c31
Janik Münzenberger <janik-muenzenberger@web.de>
2018-05-26 19:42:34 +0200

    Added auth header to api request

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,6 +5,7 @@ import { catchError } from 'rxjs/operators';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/empty';
 import { environment } from '../../environments/environment';
+import { AuthService } from './auth.service';
 
 
 @Injectable()
@@ -12,31 +13,31 @@ export class ApiService {
 
   public static API_URL: string = environment.API_HOST + "/api/";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private authService: AuthService) { }
 
   get(serviceUrl: string):Observable<any>{
-    return this.httpClient.get(ApiService.API_URL + serviceUrl)
+    return this.httpClient.get(ApiService.API_URL + serviceUrl, {headers: this.authService.authHeader()})
       .pipe(
         catchError(this.handleUnauthorizedError)
       );
   }
 
   post(serviceUrl: string, data:any):Observable<any>{
-    return this.httpClient.post(ApiService.API_URL + serviceUrl, data)
+    return this.httpClient.post(ApiService.API_URL + serviceUrl, data, {headers: this.authService.authHeader()})
       .pipe(
         catchError(this.handleUnauthorizedError)
       );
   }
 
   patch(serviceUrl: string, data:any):Observable<any>{
-    return this.httpClient.patch(ApiService.API_URL + serviceUrl, data)
+    return this.httpClient.patch(ApiService.API_URL + serviceUrl, data, {headers: this.authService.authHeader()})
       .pipe(
         catchError(this.handleUnauthorizedError)
       );
   }
 
   delete(serviceUrl: string):Observable<any>{
-    return this.httpClient.delete(ApiService.API_URL + serviceUrl)
+    return this.httpClient.delete(ApiService.API_URL + serviceUrl, {headers: this.authService.authHeader()})
       .pipe(
         catchError(this.handleUnauthorizedError)
       );
@@ -44,8 +45,7 @@ export class ApiService {
   
   private handleUnauthorizedError(err){
     if(err.status === 401){
-      console.log("Logout");
-      // Logout
+      this.authService.logout();
       return Observable.empty();
     }
     return Observable.throw(err);
