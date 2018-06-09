4913382d17ed87257b3527c4ca32dac0936e50a8
Janik Münzenberger <janik-muenzenberger@web.de>
2018-06-09 14:58:06 +0200

    Added emails

---
+++ src/app/components/email/email.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { EmailService } from '../../services/email.service';
+import { IEmail } from '../../model/IEmail';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-email',
+  templateUrl: './email.component.html',
+  styleUrls: ['./email.component.css'],
+  providers: [EmailService]
+})
+export class EmailComponent implements OnInit {
+
+  @Input() receivers: {email: string, name: string}[];
+  @Input() message: string = "";
+  emailForm: FormGroup;
+  constructor(private emailService: EmailService,
+          private userService: UserService) { }
+
+  ngOnInit() {
+    let user = this.userService.getUser();
+    this.emailForm = new FormGroup(
+      {
+        'email_receiver': new FormControl(null, [Validators.email, Validators.required]),
+        'email_sender': new FormControl(user.email, [Validators.required, Validators.email, Validators.minLength(8)]),
+        'email_message': new FormControl(null, [Validators.required])
+      }
+    );
+  }
+
+  send(){
+
+    if(this.emailForm.valid){
+      let email: IEmail = {
+        sender: this.emailForm.get('email_sender').value,
+        receiver: this.emailForm.get('email_receiver').value,
+        text: this.emailForm.get('email_message').value + "\n\n" + this.message
+      };
+      this.emailService.sendEmail(email)
+        .then(x => console.log("sucessfully sent"))
+        .catch(err => console.log(err));
+    }
+  }
+}
---
+++ src/app/services/email.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { IEmail } from '../model/IEmail';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+
+  constructor(private apiService: ApiService) { }
+
+  sendEmail(email: IEmail){
+    return this.apiService.post("email", email).toPromise();
+  }
+}
