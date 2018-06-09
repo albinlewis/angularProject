720f032542010bff76cc705c0fc3251476acf889
Janik Münzenberger <janik-muenzenberger@web.de>
2018-06-09 15:07:14 +0200

    Correction of route

--- src/backend/controller/routes.js
+++ src/backend/controller/routes.js
@@ -24,7 +24,7 @@ router.post('/gardeners', gardenerController.postGardener);
 
 router.post('/notification', notificationController.postNotification);
 
-router.get('/email', emailController.email);
+router.post('/email', middleware.checkBody(["sender", "receiver", "text"]), emailController.email);
 
 router.post('/analysis',
       middleware.checkBody(["crop_id"]),
