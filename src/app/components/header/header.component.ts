ec7bc64e40453fe29f78d5817176d32ca98fe6dd
Janik Münzenberger <janik-muenzenberger@web.de>
2018-06-09 14:55:50 +0200

    Refactored structure

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import * as $ from 'jquery';
-import {AuthService} from '../services/auth.service';
+import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
 import {MatSnackBar} from '@angular/material';
 
