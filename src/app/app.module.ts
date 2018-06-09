99743c589826d47563cc4684903fd41d7a225740
Janik Münzenberger <janik-muenzenberger@web.de>
2018-06-09 14:56:42 +0200

    Update to Angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import {MatIconModule, MatSnackBarModule, MatTabsModule} from '@angular/material
 import { CdkTableModule } from '@angular/cdk/table';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import { TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { AgmCoreModule } from '@agm/core';
 
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 import { AnalyseComponent } from './analyse/analyse.component';
 import { PlantsComponent } from './plants/plants.component';
@@ -21,29 +23,33 @@ import { ProfileComponent } from './profile/profile.component';
 import { PlantdetailsComponent } from './plantdetails/plantdetails.component';
 import { ResultComponent } from './result/result.component';
 import { DiseaseComponent } from './disease/disease.component';
-import { FilterPipe } from './Pipes/filter.pipe';
-import { ShortenPipe } from './Pipes/shorten.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
+import { ShortenPipe } from './pipes/shorten.pipe';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ApiService } from './services/api.service';
 import { DataService } from './services/data.service';
 
 import { AppRoutingModule } from './app-routing.module';
-import { SortPipe } from './Pipes/sort.pipe';
+import { SortPipe } from './pipes/sort.pipe';
 import { AuthService } from './services/auth.service';
 import { AuthguardService} from './services/authguard.service';
 import { UserService } from './services/user.service';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { DiseaseListComponent } from './disease-list/disease-list.component';
 import { AnalysisService } from './services/analysis.service';
 import { GardernerComponent } from './garderner/garderner.component';
-import {LocationService} from './services/location.service';
 import { MapComponent } from './garderner/map/map.component';
 import { ListmapComponent } from './garderner/listmap/listmap.component';
 import {MapService} from './services/map.service';
+import { EmailComponent } from './components/email/email.component';
+import { GardenerService } from './services/gardener.service';
+import { StringifyPipe } from './pipes/stringify.pipe';
 
 
-
+export function HttpLoaderFactory(http: HttpClient){
+    return new TranslateHttpLoader(http);
+}
 
 
 
@@ -51,6 +57,7 @@ import {MapService} from './services/map.service';
     declarations: [
         AppComponent,
         HeaderComponent,
+        EmailComponent,
         AnalyseComponent,
         PlantsComponent,
         ProfileComponent,
@@ -67,6 +74,7 @@ import {MapService} from './services/map.service';
         GardernerComponent,
         MapComponent,
         ListmapComponent,
+        StringifyPipe,
 
     ],
     imports: [
@@ -75,12 +83,19 @@ import {MapService} from './services/map.service';
         AppRoutingModule,
         HttpClientModule,
         ReactiveFormsModule,
-        MatTabsModule,
-        MatIconModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        }),
         AgmCoreModule.forRoot({
          apiKey: 'AIzaSyDz276iYbdh-7iQuF7PFUadZa56J0--IYg'
         }),
-
+        
+        MatTabsModule,
+        MatIconModule,
         FormsModule,
         BrowserAnimationsModule,
         NoopAnimationsModule,
@@ -89,7 +104,7 @@ import {MapService} from './services/map.service';
         ReactiveFormsModule,
         environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : [],
     ],
-    providers: [AnalysisService, ApiService, AuthService, AuthguardService, UserService, LocationService, MapService],
+    providers: [AnalysisService, ApiService, AuthService, AuthguardService, UserService, GardenerService, MapService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
