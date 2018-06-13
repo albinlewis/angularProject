1a34336ee8a26d8afacee85a01df86740db245ed
Janik Münzenberger <janik-muenzenberger@web.de>
2018-06-13 12:48:06 +0200

    Converted images to webP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,6 +46,7 @@ import { EmailComponent } from './components/email/email.component';
 import { GardenerService } from './services/gardener.service';
 import { StringifyPipe } from './pipes/stringify.pipe';
 import { LanguageSwitchComponent } from './components/language-switch/language-switch.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 
 export function HttpLoaderFactory(http: HttpClient){
@@ -77,7 +78,7 @@ export function HttpLoaderFactory(http: HttpClient){
         ListmapComponent,
         StringifyPipe,
         LanguageSwitchComponent,
-
+        NotificationsComponent
     ],
     imports: [
 
