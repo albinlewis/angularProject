3384b8f5e1154448e5ffec65926381b707de5072
Janik Münzenberger <janik-muenzenberger@web.de>
2018-06-15 23:49:36 +0200

    Added autocompletes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,28 +6,16 @@ import {NotificationService} from './services/notification.service';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
     readonly VAPID_PUBLIC_KEY = 'BNaGf2POUom9qpnI45OSE8gmzjeqdvk-4HoV7Is-3RjPeCWMtgcukwEVPp0K2xMdfmSrGCS2be5rFIYX2qRwoEc';
 
-    constructor(translate: TranslateService) {
+    constructor(translate: TranslateService,
+            private notificationService: NotificationService) {
         translate.setDefaultLang('en');
         translate.use('en');
         // this.subscribeToNotifications();
-    }
-
-
-    subscribeToNotifications() {
-
-        this.swPush.requestSubscription({
-            serverPublicKey: this.VAPID_PUBLIC_KEY
-        })
-            .then(sub => {
-                this.notificationservice.addPushSubscriber(sub).subscribe();
-                console.log(sub);
-            })
-            .catch(err => console.error('Could not subscribe to notifications', err));
     }
 
 }
